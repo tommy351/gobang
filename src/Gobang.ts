0bd37b60aef20317558fb550db01436914fb54eb
Tommy Chen <tommy351@gmail.com>
2018-01-01 23:28:10 +0800

    Refactor gobang algorithm

--- src/Gobang.ts
+++ src/Gobang.ts
@@ -52,8 +52,10 @@ export class Gobang {
   }
 
   public updateCell(x: number, y: number) {
+    // Do nothing if there's a winner
     if (this.winner) return;
 
+    // Do nothing if the cell is not empty
     const prevStatus = this.cells[x][y];
     if (prevStatus !== CellStatus.Empty) return;
 
@@ -64,10 +66,12 @@ export class Gobang {
       status
     };
 
+    // Update the state
     this.cells[x][y] = status;
     this.winner = this.getWinner(cell);
     this.player = this.getNextPlayer();
 
+    // Emit to listeners
     for (const listener of this.listeners) {
       listener(cell, prevStatus);
     }
@@ -76,6 +80,7 @@ export class Gobang {
   public subscribe(listener: Listener): DisposeFunction {
     this.listeners.push(listener);
 
+    // Return a function for disposing
     return () => {
       for (let i = 0; i < this.listeners.length; i++) {
         if (this.listeners[i] === listener) {
@@ -87,39 +92,36 @@ export class Gobang {
   }
 
   private getWinner({ x, y }: Position): Player | undefined {
+    const rangeX = range(x - 4, x + 4);
+    const rangeY = range(y - 4, y + 4);
+
     return (
       // Check |
-      this.checkSeq(makeSeq(y - 4, y + 4).map(y => ({ x, y }))) ||
+      this.checkRange(rangeY.map(y => ({ x, y }))) ||
       // Check -
-      this.checkSeq(makeSeq(x - 4, x + 4).map(x => ({ x, y }))) ||
+      this.checkRange(rangeX.map(x => ({ x, y }))) ||
       // Check /
-      this.checkSeq(
-        zipPosition(makeSeq(x - 4, x + 4), makeSeq(y - 4, y + 4).reverse())
-      ) ||
+      this.checkRange(zip(rangeX, reverse(rangeY))) ||
       // Check \
-      this.checkSeq(zipPosition(makeSeq(x - 4, x + 4), makeSeq(y - 4, y + 4)))
+      this.checkRange(zip(rangeX, rangeY))
     );
   }
 
-  private checkSeq(seq: Position[]): Player | undefined {
+  private checkRange(range: Position[]): Player | undefined {
     const { column, row } = this.size;
-    let arr = seq
+    const arr = range
+      // Filter out-of-range elements
       .filter(({ x, y }) => x >= 0 && x < column && y >= 0 && y < row)
+      // Get the cell status
       .map(({ x, y }) => this.cells[x][y]);
 
-    arr = trimArray(arr);
-    if (arr.length < 5) return;
+    // Find the chunk with 5 continous elements
+    const chunks = chunkByDiff(arr).filter(
+      chunk => chunk[0] && chunk.length >= 5
+    );
 
-    let idx = 0;
-
-    for (let i = 1; i < arr.length; i++) {
-      if (arr[i] !== arr[idx]) {
-        idx = i;
-      }
-
-      if (i - idx >= 4) {
-        return arr[i] as any;
-      }
+    if (chunks.length) {
+      return chunks[0][0] as Player;
     }
 
     return;
@@ -134,45 +136,45 @@ export class Gobang {
   }
 }
 
-function makeSeq(from: number, to: number): number[] {
+function range(start: number, end: number, step: number = 1): number[] {
   const result: number[] = [];
 
-  for (let i = from; i <= to; i++) {
+  if (start > end) {
+    for (let i = start; i >= end; i += step) {
+      result.push(i);
+    }
+
+    return result;
+  }
+
+  for (let i = start; i <= end; i += step) {
     result.push(i);
   }
 
   return result;
 }
 
-function trimArray<T>(arr: T[]): T[] {
-  let start = 0;
-  let end = arr.length;
-
-  // Trim left
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i]) {
-      start = i;
-      break;
-    }
-  }
-
-  // Trim right
-  for (let i = start; i < arr.length; i++) {
-    if (!arr[i]) {
-      end = i;
-      break;
-    }
-  }
-
-  return arr.slice(start, end);
+function zip(x: number[], y: number[]): Position[] {
+  return x.map((x, i) => ({ x, y: y[i] }));
 }
 
-function zipPosition(arrX: number[], arrY: number[]): Position[] {
-  const result: Position[] = [];
+function reverse<T>(arr: T[]): T[] {
+  return arr.slice().reverse();
+}
 
-  for (let i = 0; i < arrX.length; i++) {
-    result.push({ x: arrX[i], y: arrY[i] });
-  }
+function chunkByDiff<T>(arr: T[]): T[][] {
+  return arr.reduce(
+    (acc, element) => {
+      const lastGroup = acc[acc.length - 1];
 
-  return result;
+      if (lastGroup && lastGroup[0] === element) {
+        lastGroup.push(element);
+      } else {
+        acc.push([element]);
+      }
+
+      return acc;
+    },
+    [] as T[][]
+  );
 }
