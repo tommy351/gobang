9b412da29625c62e35030b1f42cfdc5d2d36f2f3
Tommy Chen <tommy351@gmail.com>
2018-01-02 00:23:46 +0800

    Use extract-text-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,26 +3,24 @@ const { CheckerPlugin } = require("awesome-typescript-loader");
 const { join } = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const HtmlWebpackHarddiskPlugin = require("html-webpack-harddisk-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const DEV_MODE = process.env.NODE_ENV === "development";
 
-const plugins = [
-  new webpack.EnvironmentPlugin("NODE_ENV"),
-  new CheckerPlugin(),
-  new HtmlWebpackPlugin({
-    title: "Gobang",
-    filename: "index.html",
-    template: "templates/index.html"
-  }),
-  new HtmlWebpackHarddiskPlugin(),
-  new webpack.NamedModulesPlugin()
+const cssLoader = [
+  {
+    loader: "css-loader",
+    options: {
+      modules: true,
+      localIdentName: "[name]__[local]--[hash:base64:5]",
+      sourceMap: true,
+      importLoaders: 1
+    }
+  },
+  "postcss-loader"
 ];
 
-if (DEV_MODE) {
-  plugins.push(new webpack.HotModuleReplacementPlugin());
-}
-
-module.exports = {
+const config = {
   devtool: DEV_MODE ? "eval" : "source-map",
   entry: {
     main: ["./src/index.ts"]
@@ -48,24 +46,42 @@ module.exports = {
         test: /\.ts$/,
         loader: "awesome-typescript-loader",
         exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
-              localIdentName: "[name]__[local]--[hash:base64:5]",
-              sourceMap: true,
-              importLoaders: 1
-            }
-          },
-          "postcss-loader"
-        ]
       }
     ]
   },
-  plugins
+  plugins: [
+    new webpack.EnvironmentPlugin("NODE_ENV"),
+    new CheckerPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Gobang",
+      filename: "index.html",
+      template: "templates/index.html"
+    }),
+    new HtmlWebpackHarddiskPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
 };
+
+if (DEV_MODE) {
+  config.plugins.push(new webpack.HotModuleReplacementPlugin());
+  config.module.rules.push({
+    test: /\.css$/,
+    use: ["style-loader", ...cssLoader]
+  });
+} else {
+  config.plugins.push(
+    new ExtractTextPlugin({
+      filename: "[name]-[contenthash:8].css",
+      allChunks: true
+    })
+  );
+  config.module.rules.push({
+    test: /\.css$/,
+    use: ExtractTextPlugin.extract({
+      fallback: "style-loader",
+      use: cssLoader
+    })
+  });
+}
+
+module.exports = config;
